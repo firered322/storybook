47fc1be77bfb1a925a28169b1c3be67f281cd9ac
Atul <atulsingh1396@gmail.com>
2020-06-21 16:01:58 +0530

    added EJS scaffold

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 // Load config
 dotenv.config({ path: "./config/config.env" });
 
+connectDB();
+
 const app = express();
+
+app.set("view engine", "ejs");
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.get("/", (req, res) => {
+  res.render("pages/index");
+});
 
 const PORT = process.env.PORT || 5000;
 
