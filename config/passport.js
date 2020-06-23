34e05c1034654314c5fe3ec3b09610438bf174a5
Atul <atulsingh1396@gmail.com>
2020-06-23 11:10:00 +0530

    added passport config

---
+++ config/passport.js
@@ -0,0 +1,54 @@
+const passport = require("passport");
+
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const User = require("../models/User");
+
+module.exports = function (passport) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "/auth/google/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        const newUser = {
+          googleId: profile.id,
+          displayName: profile.displayName,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          image: profile.photos[0].value,
+        };
+
+        try {
+          // find if a user exists
+          let user = await User.findOne({ googleID: profile.id });
+          if (user) {
+            done(null, user);
+          } else {
+            user = await User.create(newUser);
+            done(null, user);
+          }
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    )
+  );
+
+  /*
+   * Each subsequent request will not contain credentials, but rather the unique cookie
+   * that identifies the session. In order to support login sessions,
+   * Passport will serialize and deserialize user instances to and from the session.
+   */
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
+};
