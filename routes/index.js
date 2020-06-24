124b6a023416ba915a0ccf3b99597ced777fae3c
Atul <atulsingh1396@gmail.com>
2020-06-24 11:57:53 +0530

    added Story model and error pages

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { verifyAuthenticated, verifyGuest } = require("../middleware/auth");
 
+const Story = require("../models/Story");
+
 // @desc Home login page
 // @route GET /
 router.get("/", verifyGuest, (req, res) => {
@@ -10,8 +12,15 @@ router.get("/", verifyGuest, (req, res) => {
 
 // @desc Loggedin Dashboard page
 // @route GET /dashboard
-router.get("/dashboard", verifyAuthenticated, (req, res) => {
-  res.render("pages/dashboard");
+router.get("/dashboard", verifyAuthenticated, async (req, res) => {
+  try {
+    // find User's stories
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render("pages/dashboard", { name: req.user.firstName, stories });
+  } catch (err) {
+    console.error(err);
+    res.render("/error/500");
+  }
 });
 
 module.exports = router;
