ad49f03683c46a6bb7aaf24fdf5db1c632a946e6
Atul <atulsingh1396@gmail.com>
2020-06-30 18:46:25 +0530

    method override package for handling story changes using PUT and changes in dashboard for edit and delete

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const passport = require("passport");
 const session = require("express-session");
+const methodOverride = require("method-override");
 const MongoStore = require("connect-mongo")(session);
 const mongoose = require("mongoose");
 const connectDB = require("./config/db");
@@ -21,6 +22,17 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
 // Set EJS
 app.set("view engine", "ejs");
 app.set("views", require("path").join(__dirname, "views"));
