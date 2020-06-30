15bc75133d0e840c9678ae5250ff7efacbbc03af
Atul <atulsingh1396@gmail.com>
2020-06-30 14:15:23 +0530

    added edit story template and endpoint

--- routes/stories.js
+++ routes/stories.js
@@ -49,4 +49,20 @@ router.get("/", verifyAuthenticated, async (req, res) => {
   }
 });
 
+// @desc Story edit page
+// @route GET /stories/edit/:id
+router.get("/edit/:id", verifyAuthenticated, async (req, res) => {
+  const story = await Story.findOne({ _id: req.params.id }).lean();
+  if (!story) {
+    return res.render("error/404");
+  }
+  if (story.user != req.user.id) {
+    res.redirect("/stories");
+  } else {
+    res.render("stories/edit", {
+      story,
+    });
+  }
+});
+
 module.exports = router;
